55f0d834b244fa4973e89f1340d51a7dbb6275df
HasanCoder66 <hasanashraf874@gmail.com>
2024-08-08 06:41:27 +0500

    just remaining

--- Branding-Frontend/src/components/BlogCard.jsx
+++ Branding-Frontend/src/components/BlogCard.jsx
@@ -1,3 +1,59 @@
+// import React from "react";
+// import Button from "react-bootstrap/Button";
+// import Card from "react-bootstrap/Card";
+// import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+// // import blogImg1 from "../assets/images/blogImg1.jpeg";
+
+// const BlogCard = ({ name, desc, imgUrl }) => {
+//   return (
+//     <>
+//       <div className="w-[100%] bg-slate-200 object-contain overflow-hidden">
+//         <Card.Img
+//           variant="top"
+//           //   src={blogImg1}
+//           src="https://images.pexels.com/photos/416405/pexels-photo-416405.jpeg?auto=compress&cs=tinysrgb&w=600"
+//           className="group-hover/item:rotate-3 duration-300 ease-out scale-110"
+//         />
+//       </div>
+//       <div className="p-[10px] flex justify-between flex-col">
+//         <div className="group-hover/item:opacity-0 duration-300 ease-out">
+//           <Card.Text className="text-theme-black text-lg my-2">
+//             By: <span className="text-theme-red">Name</span> / 2024-03-4
+//           </Card.Text>
+//         </div>
+//         <div className="group-hover/item:-translate-y-[3rem] relative duration-300 ease-out z-10">
+//           <Card.Title className="text-theme-red text-[2.3rem] font-bold my-5">
+//             {name || "hasan"}
+//           </Card.Title>
+//           <Card.Text className="text-theme-black text-xl my-4 mb-[0]">
+//             {desc ||
+//               "Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet "}
+//           </Card.Text>
+//         </div>
+//         <div className="">
+//           <Button
+//             variant="primary"
+//             className="text-white text-lg group-hover/item:text-theme-red duration-300 ease-out -translate-y-4"
+//           >
+//             Read More <ArrowForwardIcon />
+//           </Button>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default BlogCard;
+
+
+
+
+
+
+
+
+
+
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
@@ -6,70 +62,35 @@ import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 const BlogCard = ({ name, desc, imgUrl }) => {
   return (
-    <>
-      <div className="w-[100%] bg-slate-200 object-contain overflow-hidden">
-        <Card.Img
-          variant="top"
+    <Card className="w-[100%] bg-slate-200 object-contain overflow-hidden">
+      <Card.Img
+        variant="top"
         //   src={blogImg1}
-          src="https://images.pexels.com/photos/416405/pexels-photo-416405.jpeg?auto=compress&cs=tinysrgb&w=600"
-          className="group-hover/item:rotate-3 duration-300 ease-out scale-110"
-        />
-      </div>
-      <div className="p-[10px] flex justify-between flex-col">
-        <div className="group-hover/item:opacity-0 duration-300 ease-out">
-          <Card.Text className="text-theme-black text-lg my-2">
-            By: <span className="text-theme-red">Name</span> / 2024-03-4
-          </Card.Text>
-        </div>
+        src="https://images.pexels.com/photos/416405/pexels-photo-416405.jpeg?auto=compress&cs=tinysrgb&w=600"
+        className="group-hover/item:rotate-3 duration-300 ease-out scale-110"
+      />
+      <Card.Body className="p-[10px] flex justify-between flex-col">
+        <Card.Text className="text-theme-black text-base my-2 group-hover/item:opacity-0 duration-300 ease-out">
+          By: <span className="text-theme-red">Name</span> / 2024-03-4
+        </Card.Text>
         <div className="group-hover/item:-translate-y-[3rem] relative duration-300 ease-out z-10">
-          <Card.Title className="text-theme-red text-[2.3rem] font-bold my-5">
-            {name || 'hasan'}
+          <Card.Title className="text-theme-red text-2xl font-bold my-5">
+            {name || "hasan"}
           </Card.Title>
-          <Card.Text className="text-theme-black text-xl my-4 mb-[0]">
-            {desc || 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non culpa, dolore ipsum pariatur eius vero velit? Eveniet quo debitis laudantium nihil illo facere eius quod?'}
+          <Card.Text className="text-theme-black text-lg my-4 mb-0">
+            {desc ||
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet."}
           </Card.Text>
         </div>
-        <div className="">
-          <Button
-            variant="primary"
-            className="text-white text-lg group-hover/item:text-theme-red duration-300 ease-out -translate-y-4"
-          >
-
-            Read More <ArrowForwardIcon />
-          </Button>
-        </div>
-      </div>
-    </>
+        <Button
+          variant="primary"
+          className="text-white text-base group-hover/item:text-theme-red duration-300 ease-out -translate-y-4"
+        >
+          Read More <ArrowForwardIcon />
+        </Button>
+      </Card.Body>
+    </Card>
   );
 };
 
 export default BlogCard;
-
-
-
-// import Card from 'react-bootstrap/Card';
-// import Col from 'react-bootstrap/Col';
-// import Row from 'react-bootstrap/Row';
-
-// function BlogCard() {
-//   return (
-//     <Row xs={1} md={2} className="g-4">
-      
-//         <Col>
-//           <Card>
-//             <Card.Img variant="top" src="https://images.pexels.com/photos/416405/pexels-photo-416405.jpeg?auto=compress&cs=tinysrgb&w=600" />
-//             <Card.Body>
-//               <Card.Title>Card title</Card.Title>
-//               <Card.Text>
-//                 This is a longer card with supporting text below as a natural
-//                 lead-in to additional content. This content is a little bit
-//                 longer.
-//               </Card.Text>
-//             </Card.Body>
-//           </Card>
-//         </Col>
-//     </Row>
-//   );
-// }
-
-// export default BlogCard;
--- Branding-Frontend/src/pages/PortfolioPage/Portfolio.jsx
+++ Branding-Frontend/src/pages/PortfolioPage/Portfolio.jsx
@@ -16,21 +16,20 @@ const Portfolio = () => {
       </Helmet>
       <div className="flex flex-wrap justify-evenly min-h-[100vh] py-[90px] landingContainer">
         <CardGroup>
-          <BlogCard
-            name="Card title 1"
-            desc="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
-            // imgUrl={blogImg1}
-          />
-          {/* <BlogCard
-            name="Card title 2"
-            desc="This card has supporting text below as a natural lead-in to additional content."
-            // imgUrl={blogImg1}
-          />
-          <BlogCard
-            name="Card title 3"
-            desc="This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action."
-            // imgUrl={blogImg1}
-          /> */}
+        <div className="blogCardCont p-16 flex flex-wrap items-center justify-between ">
+        {/* {blogCardData.map(({ name, desc, imgUrl }, index) => (
+          ))} */}
+          <div
+            // key={index}
+            style={{ width: "307px", height: "381px" }}
+            className="bg-[#ffffff] group/item overflow-hidden mb-10 cursor-pointer"
+          >
+            {/* name={name} desc={desc} imgUrl={imgUrl} */}
+            <BlogCard  />
+            <BlogCard  />
+            <BlogCard  />
+          </div>
+      </div>
         </CardGroup>
       </div>
     </>
@@ -38,3 +37,39 @@ const Portfolio = () => {
 };
 
 export default Portfolio;
+
+
+
+
+
+// import React from "react";
+// import { BlogCard } from "../../components";
+// // import { blogCardData } from "../dummyData";
+
+
+// const Portfolio = () => {
+//   return (
+//     <div className="bg-[#FEF6F5] my-20">
+//       <div className="text-center py-[5rem]">
+//         <p className="text-theme-yellow text-[2.3rem]">LATEST NEWS</p>
+//         <p className="text-theme-red text-[3.5rem] font-semibold">
+//           Our Latest Articles
+//         </p>
+//       </div>
+//       <div className="blogCardCont p-16 flex flex-wrap items-center justify-between ">
+//         {/* {blogCardData.map(({ name, desc, imgUrl }, index) => (
+//           ))} */}
+//           <div
+//             // key={index}
+//             style={{ width: "307px", height: "381px" }}
+//             className="bg-[#ffffff] group/item overflow-hidden mb-10 cursor-pointer"
+//           >
+//             {/* name={name} desc={desc} imgUrl={imgUrl} */}
+//             <BlogCard  />
+//           </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Portfolio;
