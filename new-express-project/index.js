b3cc319194cea14117a2d303aa11e30ca854d2fb
Hasan Coder 66 <hasanashraf874@gmail.com>
2024-09-09 21:45:32 +0500

    index changes wirte 1 api for / checking

--- new-express-project/index.js
+++ new-express-project/index.js
@@ -1,57 +1,59 @@
-    import express from "express";
-    import mongoose from "mongoose";
-    import cookieParser from "cookie-parser";
-    import bodyParser from "body-parser";
-    import dotenv from "dotenv";
-    import cors from "cors";
-    import UserRoute from "./routes/userRoute.js";
-    import BlogRoute from "./routes/blogRoutes.js";
+import express from "express";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+import UserRoute from "./routes/userRoute.js";
+import BlogRoute from "./routes/blogRoutes.js";
 
-    dotenv.config();
-    const app = express();
-    const PORT = process.env.PORT || 5500;
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5500;
 
-    const connectDB = () => {
-    mongoose
-        .connect(process.env.MONGO_URI)
-        .then(() => {
-        console.log("Database Connected");
-        })
-        .catch((error) => {
-        throw error;
-        });
-    };
-
-    app.use(cookieParser());
-    app.use(express.json());
-    app.use(bodyParser.json());
-    app.use(
-    cors({
-        credentials: true,
-        origin: "*",
-        methods: ["POST", "GET", "PUT", "DELETE"],
+const connectDB = () => {
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log("Database Connected");
     })
-    );
-
-    app.use("/api/auth", UserRoute);
-    app.use("/api/blog", BlogRoute);
-
-    app.use((err, req, res, next) => {
-    const errorStatus = err.status || 500;
-    const errorMessage = err.message || "Something went wrong!";
-    const errorStack = err.stack || "No stack trace available";
-
-    console.error("error stack", errorStack);
-    console.error("error Message", errorMessage);
-
-    return res.status(errorStatus).json({
-        status: errorStatus,
-        message: errorMessage,
-        stack: errorStack,
+    .catch((error) => {
+      throw error;
     });
-    });
+};
 
-    app.listen(PORT, () => {
-    connectDB();
-    console.log(`Server listening on port ${PORT}`);
-    });
+app.use(cookieParser());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: "*",
+    methods: ["POST", "GET", "PUT", "DELETE"],
+  })
+);
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.use("/api/auth", UserRoute);
+app.use("/api/blog", BlogRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  const errorStack = err.stack || "No stack trace available";
+
+  console.error("error stack", errorStack);
+  console.error("error Message", errorMessage);
+
+  return res.status(errorStatus).json({
+    status: errorStatus,
+    message: errorMessage,
+    stack: errorStack,
+  });
+});
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server listening on port ${PORT}`);
+});
