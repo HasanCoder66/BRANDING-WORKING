0aeb5c1e9acbe0e86d74eaf9ad914b342332563e
Hasan Coder 66 <hasanashraf874@gmail.com>
2024-09-12 17:26:37 +0500

    new changes for all data

--- new-express-project/index.js
+++ new-express-project/index.js
@@ -1,42 +1,42 @@
-// import express from "express";
-// import mongoose from "mongoose";
-// import cookieParser from "cookie-parser";
-// import bodyParser from "body-parser";
-// import dotenv from "dotenv";
-// import cors from "cors";
-// // import UserRoute from "./routes/userRoute.js";
-// // import BlogRoute from "./routes/blogRoutes.js";
+import express from "express";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+import UserRoute from "./routes/userRoute.js";
+import BlogRoute from "./routes/blogRoutes.js";
 
-// dotenv.config();
-// const app = express();
-// const PORT = process.env.PORT || 5500;
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5500;
 
-// const connectDB = () => {
-//   mongoose
-//     .connect(process.env.MONGO_URI)
-//     .then(() => {
-//       console.log("Database Connected");
-//     })
-//     .catch((error) => {
-//       throw error;
-//     });
-// };
+const connectDB = () => {
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log("Database Connected");
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
 
-// app.use(cookieParser());
-// app.use(express.json());
-// app.use(bodyParser.json());
-// app.use(
-//   cors({
-//     credentials: true,
-//     origin: "*",
-//     methods: ["POST", "GET", "PUT", "DELETE"],
-//   })
-// );
-// app.get("/", (req, res) => {
-//   res.send("Hello World");
-// });
-// // app.use("/api/auth", UserRoute);
-// // app.use("/api/blog", BlogRoute);
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: "*",
+    methods: ["POST", "GET", "PUT", "DELETE"],
+  })
+);
+app.get("/", (req, res) => {
+  res.send("Hello from Express on Vercel!");
+});
+app.use("/api/auth", UserRoute);
+app.use("/api/blog", BlogRoute);
 
 // app.use((err, req, res, next) => {
 //   const errorStatus = err.status || 500;
@@ -53,22 +53,10 @@
 //   });
 // });
 
-// app.listen(PORT, () => {
-//   connectDB();
-//   console.log(`Server listening on port ${PORT}`);
-// });
-
-
-
-import express from "express";
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello from Express on Vercel!");
-});
-
-const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  connectDB();
+  console.log(`Server listening on port ${PORT}`);
 });
+
+
+
