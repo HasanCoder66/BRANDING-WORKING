72a538ab469aae6a3c9ed9e9f0fb306091e7e427
Hasan Coder 66 <hasanashraf874@gmail.com>
2024-09-25 15:40:29 +0500

    services page remove extra code and seo tricks done

--- Branding-Frontend/src/components/BlogCard.jsx
+++ Branding-Frontend/src/components/BlogCard.jsx
@@ -4,35 +4,39 @@ import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import { Link } from "react-router-dom";
 
 const BlogCard = ({ data }) => {
-  // console.log(CardData)
   const { id, desc, imgUrl, title, buttonContent, routeLink } = data;
   return (
     <Link to={routeLink}>
-    <Card key={id} className="  object-contain overflow-hidden border-2px-[#fca311]">
-      <Card.Img
-        // variant="top"
-        src={imgUrl}
-        className="group-hover/item:rotate-3 duration-300 ease-out scale-110"
-      />
-      <Card.Body className="p-[10px] flex justify-between flex-col">
-        <div className="group-hover/item:-translate-y-[2rem] relative duration-300 ease-out z-10">
-          <Card.Title className="text-theme-red text-2xl font-bold my-5">
-            {title}
-          </Card.Title>
-          <Card.Text className="text-theme-black text-md my-4 mb-0">
-            {desc}
-          </Card.Text>
-        </div>
-        <Link to={routeLink}>
-          <div
-            variant="primary"
-            className="mt-[10px] text-white text-base group-hover/item:text-theme-red duration-300 ease-out -translate-y-8"
-          >
-            {buttonContent} <ArrowForwardIcon />
+      <Card
+        key={id}
+        className="  object-contain overflow-hidden border-2px-[#fca311]"
+      >
+        <Card.Img
+          width={826}
+          height={551}
+          src={imgUrl}
+          alt="Services-Cards"
+          className="group-hover/item:rotate-3 duration-300 ease-out scale-110"
+        />
+        <Card.Body className="p-[10px] flex justify-between flex-col">
+          <div className="group-hover/item:-translate-y-[2rem] relative duration-300 ease-out z-10">
+            <Card.Title className="text-theme-red text-2xl font-bold my-5">
+              {title}
+            </Card.Title>
+            <Card.Text className="text-theme-black text-md my-4 mb-0">
+              {desc}
+            </Card.Text>
           </div>
-        </Link>
-      </Card.Body>
-    </Card>
+          <Link to={routeLink}>
+            <div
+              variant="primary"
+              className="mt-[10px] text-white text-base group-hover/item:text-theme-red duration-300 ease-out -translate-y-8"
+            >
+              {buttonContent} <ArrowForwardIcon />
+            </div>
+          </Link>
+        </Card.Body>
+      </Card>
     </Link>
   );
 };
--- Branding-Frontend/src/pages/ServicesPage/Services.jsx
+++ Branding-Frontend/src/pages/ServicesPage/Services.jsx
@@ -5,69 +5,67 @@ import "aos/dist/aos.css";
 import { useEffect } from "react";
 import serviceCover from "../../assets/serviceCover.png";
 import { BlogCard } from "../../components";
+import { Helmet } from "react-helmet-async";
 
 const AllServices = () => {
   useEffect(() => {
     AOS.init({
-      // Global settings:
-      disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-      startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
-      initClassName: "aos-init", // class applied after initialization
-      animatedClassName: "aos-animate", // class applied on animation
-      useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-      disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-      debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-      throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-
-      // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-      offset: 120, // offset (in px) from the original trigger point
-      delay: 0, // values from 0 to 3000, with step 50ms
-      duration: 1000, // values from 0 to 3000, with step 50ms
-      easing: "ease", // default easing for AOS animations
-      once: true, // whether animation should happen only once - while scrolling down
-      mirror: false, // whether elements should animate out while scrolling past them
-      anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
+      disable: false,
+      startEvent: "DOMContentLoaded",
+      initClassName: "aos-init",
+      animatedClassName: "aos-animate",
+      useClassNames: false,
+      disableMutationObserver: false,
+      debounceDelay: 50,
+      throttleDelay: 99,
+      offset: 120,
+      delay: 0,
+      duration: 1000,
+      easing: "ease",
+      once: true,
+      mirror: false,
+      anchorPlacement: "top-bottom",
     });
   }, []);
   return (
-    <div className="min-h-[100vh] landingContainer py-[70px] flex flex-col justify-center items-center gap-[20px]">
-      <div data-aos="zoom-in-left" className="w-[100vw] ">
-        <img
-          // src="https://res.cloudinary.com/dpvxkqhi8/image/upload/v1710948265/branding%20hopes/service_cover_hn6ehv.jpg"
-          src={serviceCover}
-          alt="Service-Cover"
-          className="h-[50vh] w-[100%] object-cover	"
+    <>
+      <Helmet>
+        <meta
+          name="description"
+          content="Grow your business with Branding Hopes. We offer expert web development, creative design, SEO, and digital marketing services to elevate your brand and drive growth."
         />
+        <link rel="canonical" href="https://www.brandinghopes.com/services" />
+      </Helmet>
+
+      <div className="min-h-[100vh] landingContainer py-[70px] flex flex-col justify-center items-center gap-[20px]">
+        <div data-aos="zoom-in-left" className="w-[100vw] ">
+          <img
+            width={3240}
+            height={1840}
+            src={serviceCover}
+            alt="Service-Cover"
+            className="h-[50vh] w-[100%] object-cover	"
+          />
+        </div>
+        <div className="md:7/12 lg:w-6/12 flex flex-col items-center gap-[2rem] pt-[2rem]">
+          <h1 className="text-[2.5rem] tracking-widest	 text-[#fca311] uppercase text-center font-bold">
+            We Provides You These <span className="text-white">Services</span>
+          </h1>
+        </div>
+        <div className=" blogCardCont p-16 flex flex-wrap  pt-[3rem] items-center justify-evenly ">
+          {data.map((data, index) => (
+            <div
+              key={index}
+              style={{ width: "380px", height: "440px" }}
+              className="bg-[#ffffff] group/item overflow-hidden mb-10 cursor-pointer"
+            >
+              <BlogCard data={data} />
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="md:7/12 lg:w-6/12 flex flex-col items-center gap-[2rem] pt-[2rem]">
-        <h2 className="text-[2.5rem] tracking-widest	 text-[#fca311] uppercase text-center font-bold">
-          We Provides You These <span className="text-white">Services</span>
-        </h2>
-      </div>
-      <div className=" blogCardCont p-16 flex flex-wrap  pt-[3rem] items-center justify-evenly ">
-        {data.map((data, index) => (
-          <div
-            key={index}
-            style={{ width: "380px", height: "440px" }}
-            className="bg-[#ffffff] group/item overflow-hidden mb-10 cursor-pointer"
-          >
-            <BlogCard data={data} />
-            {/* name={name} desc={desc} imgUrl={imgUrl} */}
-          </div>
-        ))}
-      </div>
-    </div>
+    </>
   );
 };
 
 export default AllServices;
-
-{
-  /* <div className="">
-      <div className="flex justify-evenly flex-wrap ">
-        {data.map((data) => (
-          <AllServiceCardCmp data={data} />
-        ))}
-      </div>
-    </div> */
-}
