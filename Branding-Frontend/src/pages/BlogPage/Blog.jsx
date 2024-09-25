189de9d42b958458c595ab822c20090d2dbebe5f
Hasan Coder 66 <hasanashraf874@gmail.com>
2024-09-25 16:26:35 +0500

    create blog page done

--- Branding-Frontend/src/pages/BlogPage/Blog.jsx
+++ Branding-Frontend/src/pages/BlogPage/Blog.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { BlogCmpHead, BlogView } from "../../components";
+import { Helmet } from "react-helmet-async";
 
 const Blog = () => {
   return (
-    <div className="w-[100%] landingContainer min-h-[100vh] py-[90px]">
-      <BlogCmpHead />
-      <BlogView />
-    </div>
+    <>
+      <Helmet>
+        <meta
+          name="description"
+          content="Create engaging and impactful blogs with Branding Hopes. Share your ideas, insights, and stories to connect with your audience and grow your brand's online presence."
+        />
+        <link rel="canonical" href="https://www.brandinghopes.com/blog" />
+      </Helmet>
+
+      <div className="w-[100%] landingContainer min-h-[100vh] py-[90px]">
+        <BlogCmpHead />
+        <BlogView />
+      </div>
+    </>
   );
 };
 
