4f05b5c964a08c54406550d87ed16070b99a8f04
Hasan Coder 66 <hasanashraf874@gmail.com>
2024-09-25 16:29:26 +0500

    signup page done

--- Branding-Frontend/src/components/SignupForm.jsx
+++ Branding-Frontend/src/components/SignupForm.jsx
@@ -118,8 +118,8 @@ function SignUpForm() {
     <div className="bg-login-bg-imag w-[60%] flex justify-center ">
       <div className="flex justify-center w-full items-center gap-[4rem]  px-12 py-10">
         <form className="w-[50%]  p-10 shadow-lg bg-white   rounded-lg">
-          <h1 className="text-4xl block  font-bold text-theme-red text-center">
-            SignUp
+          <h1 className="text-2xl block  font-bold text-theme-red text-center uppercase">
+          Join Branding Hopes
             <br />
           </h1>
           <div className="mt-4 relative">
--- Branding-Frontend/src/pages/SignupPage/Signup.jsx
+++ Branding-Frontend/src/pages/SignupPage/Signup.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import { SignUpForm } from '../../components'
+import React from "react";
+import { SignUpForm } from "../../components";
+import { Helmet } from "react-helmet-async";
 
 const Signup = () => {
   return (
-    <div className="landingContainer h-screen flex justify-center items-center min-h-dvh">
-    <SignUpForm />
-    
-  </div>
-  )
-}
+    <>
+      <Helmet>
+        <meta
+          name="description"
+          content="Join Branding Hopes today! Sign up to access our digital marketing services and resources, and start your journey towards elevating your brand."
+        />
+        <link rel="canonical" href="https://www.brandinghopes.com/signup" />
+      </Helmet>
 
-export default Signup
+      <div className="landingContainer h-screen flex justify-center items-center min-h-dvh">
+        <SignUpForm />
+      </div>
+    </>
+  );
+};
+
+export default Signup;
