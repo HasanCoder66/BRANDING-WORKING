134b4d4ba87749d3ffd313566fb25ad78b4edbc6
Hasan Coder 66 <hasanashraf874@gmail.com>
2024-10-14 19:13:40 +0500

    dashboard pages added

---
+++ Branding-Frontend/src/dashboard/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BodyMain from "../components/BodyMain";
+
+function Home() {
+  return (
+    <div className="flex">
+      <BodyMain />
+    </div>
+  );
+}
+
+export default Home;
